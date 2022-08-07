91fec7c47ed4509fb832d300de6415cba08dc7b3
andrewklips <andrew.klips@gmail.com>
2022-08-07 16:43:49 +0500

    tests update

--- src/test/java/ManagerTest.java
+++ src/test/java/ManagerTest.java
@@ -18,6 +18,7 @@ public class ManagerTest {
 
         assertArrayEquals(expected, actual);
     }
+
     @Test
     public void emptyPosterTest() {
 
@@ -59,4 +60,85 @@ public class ManagerTest {
         assertArrayEquals(expected, actual);
     }
 
+    @Test
+    public void findLastPosterEqualThanLimit() {
+
+        Manager manager = new Manager(6);
+
+        manager.addFilm("film7");
+        manager.addFilm("film8");
+        manager.addFilm("film9");
+        manager.addFilm("film10");
+        manager.addFilm("film11");
+        manager.addFilm("film12");
+
+        String[] expected = {
+                "film12",
+                "film11",
+                "film10",
+                "film9",
+                "film8",
+                "film7"
+
+
+        };
+        String[] actual = manager.findLast();
+
+        assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void findLastPosterLessThanLimit() {
+        Manager manager = new Manager(7);
+
+
+        manager.addFilm("film7");
+        manager.addFilm("film8");
+        manager.addFilm("film9");
+        manager.addFilm("film10");
+        manager.addFilm("film11");
+        manager.addFilm("film12");
+
+
+        String[] expected = {
+                "film12",
+                "film11",
+                "film10",
+                "film9",
+                "film8",
+                "film7"
+
+
+        };
+        String[] actual = manager.findLast();
+
+        assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void findLastPosterMoreThanLimit() {
+        Manager manager = new Manager(5);
+
+
+        manager.addFilm("film7");
+        manager.addFilm("film8");
+        manager.addFilm("film9");
+        manager.addFilm("film10");
+        manager.addFilm("film11");
+        manager.addFilm("film12");
+
+
+        String[] expected = {
+                "film12",
+                "film11",
+                "film10",
+                "film9",
+                "film8"
+
+
+        };
+        String[] actual = manager.findLast();
+
+        assertArrayEquals(expected, actual);
+    }
 }
